4b08f37a251187e6151d4a2b10cdbb296ab8a6ac
taki0412 <taki041210@naver.com>
2022-10-20 20:23:21 +0900

    Add ContentMessage 추가 (#3)

--- recommender_system/core/models/__init__.py
+++ recommender_system/core/models/__init__.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 from pydantic import BaseModel
 from sqlalchemy import Column, String
 
+
 class ContentMessage(BaseModel):
     text: Column(String)
     images: Optional[List[Column(String)]]
