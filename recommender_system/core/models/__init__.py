8eee458b5795a63a8877f8aa4668177f30c2a204
taki0412 <taki041210@naver.com>
2022-10-20 20:31:55 +0900

    Move Category && Add FeedType (#3)

--- recommender_system/core/models/__init__.py
+++ recommender_system/core/models/__init__.py
@@ -4,7 +4,22 @@ from pydantic import BaseModel
 from sqlalchemy import Column, String
 
 
+class Category:
+    celebrity: Column(INT=0, message="연애인")
+    movie: Column(INT=1, message="영화")
+    animation: Column(INT=2, message="만화/애니")
+    web_toon: Column(INT=3, message="웹툰")
+    game: Column(INT=4, message="게임")
+    military: Column(INT=5, message="밀리터리")
+    it: Column(INT=6, message="IT")
+
+
 class ContentMessage(BaseModel):
     text: Column(String)
     images: Optional[List[Column(String)]]
     video: Optional[Column(String)]
+
+
+class FeedType:
+    duck_deal: Column(INT=0, message="덕딜")
+    duck_feed: Column(INT=1, message="덕피드")
