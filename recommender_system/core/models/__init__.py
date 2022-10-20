accb0f24d7991b4f871e84c40f48ac01da855c00
taki0412 <taki041210@naver.com>
2022-10-20 20:23:10 +0900

    Add ContentMessage 추가 (#3)

---
+++ recommender_system/core/models/__init__.py
@@ -0,0 +1,9 @@
+# Import Library
+from typing import Optional, List
+from pydantic import BaseModel
+from sqlalchemy import Column, String
+
+class ContentMessage(BaseModel):
+    text: Column(String)
+    images: Optional[List[Column(String)]]
+    video: Optional[Column(String)]
