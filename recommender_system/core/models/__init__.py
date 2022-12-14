93cf196d8cdd7d4d16c661a5f9472c7bbf680d65
taki0412 <taki041210@naver.com>
2022-12-14 22:55:43 +0900

    Add DB 접속 성공 (#3)

--- recommender_system/core/models/__init__.py
+++ recommender_system/core/models/__init__.py
@@ -1,51 +1,29 @@
-# Import Library
-from typing import Optional, List
-from pydantic import BaseModel
-from sqlalchemy import Column, String
+# Import Libraries
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+import sys
+from recommender_system.config.connect import DB_URL
 
 
-class Category:
-    celebrity: Column(INT=0, message="연애인")
-    movie: Column(INT=1, message="영화")
-    animation: Column(INT=2, message="만화/애니")
-    web_toon: Column(INT=3, message="웹툰")
-    game: Column(INT=4, message="게임")
-    military: Column(INT=5, message="밀리터리")
-    it: Column(INT=6, message="IT")
+@contextmanager
+def session_scope():
+    global DB_URL
+    engine = create_engine(
+        DB_URL,
+        encoding="utf-8"
+    )
+    Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
-class ContentMessage(BaseModel):
-    """
-    @param
-    text: 글
-    images: 이미지들 url
-    video: 영상 url
-    """
-    text: Column(String)
-    images: Optional[List[Column(String)]]
-    video: Optional[Column(String)]
-
-
-class FeedType:
-    duck_deal: Column(INT=0, message="덕딜")
-    duck_feed: Column(INT=1, message="덕피드")
-
-
-class Score:
-    """
-    @:param
-    came_feed: 피드 떴을 때
-    read: 피드 클릭 안하고 그냥 읽었을 때
-    carefully_read: 피드를 클릭해서 자세히 읽었을 때
-    heart: 좋아요 눌렀을 때
-    write_comment: 댓글을 작성했을 때
-    share: 공유 했을 때
-    no_interest: 관심 없음
-    """
-    came_feed = 0
-    read = 1
-    carefully_read = 2
-    heart = 3
-    write_comment = 5
-    share = 5
-    no_interest = -3
+Base = declarative_base()
